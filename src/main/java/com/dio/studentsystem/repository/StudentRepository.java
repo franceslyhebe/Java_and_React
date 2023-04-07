610f3d3f64e4e12e260649b54f2edd09b971412f
Francesly Hebe <f.francesly@gmail.com>
2023-04-07 20:17:08 -0300

    Criando repository Student

--- src/main/java/com/dio/studentsystem/repository/StudentRepository.java
+++ src/main/java/com/dio/studentsystem/repository/StudentRepository.java
@@ -1,3 +1,8 @@
 package com.dio.studentsystem.repository;
 
-public class StudentRepository {}
+import com.dio.studentsystem.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Integer> {}
