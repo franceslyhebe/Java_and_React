d8bdff41471fd555f773dda7882ca9816d4e5bf9
Francesly Hebe <f.francesly@gmail.com>
2023-04-07 22:54:41 -0300

    Buscar estudante no BD

--- src/main/java/com/dio/studentsystem/controller/StudentController.java
+++ src/main/java/com/dio/studentsystem/controller/StudentController.java
@@ -5,9 +5,11 @@ import com.dio.studentsystem.service.StudentService;
 import java.util.List;import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;import org.springframework.web.bind.annotation.PostMapping;import org.springframework.web.bind.annotation.RequestBody;import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
 @RequestMapping("/student")
+@CrossOrigin
 public class StudentController {
 
     @Autowired
